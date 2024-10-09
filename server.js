11f754e14c26b24bc44ba3bb853316dde6d12a36
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-10-09 10:22:44 +0530

    simplified

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 import express from "express";
-import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js";
-import connectDB from "./config/db.js";
-import cookieParser from "cookie-parser";
-dotenv.config();
-import cors from "cors";
+// import dotenv from "dotenv";
+// import userRoutes from "./routes/userRoutes.js";
+// import connectDB from "./config/db.js";
+// import cookieParser from "cookie-parser";
+// dotenv.config();
+// import cors from "cors";
 
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-const PORT = process.env.PORT || 5000;
+// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+// const PORT = process.env.PORT || 5000;
 
-connectDB();
+// connectDB();
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
-app.use(cookieParser());
+// app.use(cookieParser());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/users", userRoutes);
-app.use(notFound);
-app.use(errorHandler);
-app.get("/", async (req, res) => res.send("server is ready"));
+// app.use("/api/users", userRoutes);
+// app.use(notFound);
+// app.use(errorHandler);
+// app.get("/", async (req, res) => res.send("server is ready"));
 
 app.listen(PORT, () => console.log("server started on port ", PORT));
