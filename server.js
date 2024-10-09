73b6213263b9cdc3447702ec71950424bc66fd4a
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-10-09 10:24:01 +0530

    simplified

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from "express";
-// import dotenv from "dotenv";
-// import userRoutes from "./routes/userRoutes.js";
-// import connectDB from "./config/db.js";
-// import cookieParser from "cookie-parser";
-// dotenv.config();
-// import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
+import cors from "cors";
 
 // import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 // const PORT = process.env.PORT || 5000;
 
 // connectDB();
 
-// const app = express();
+const app = express();
 // app.use(cors());
 
 // app.use(cookieParser());
