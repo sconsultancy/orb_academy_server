bec7e7aaa92057649ca6857f4487b93001143ff0
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-10-09 10:34:05 +0530

    crrct 5

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 // app.use(notFound);
